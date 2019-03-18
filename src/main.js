dcbf450bf5d567c3680f66a453284f5df6b8939a
Darkzarich <darkzarichv2@gmail.com>
2019-03-19 01:22:45 +0300

    bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   store,
