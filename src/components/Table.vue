5a5f9a85df3b3b44240ac26178031ac7a8679054
Darkzarich <darkzarichv2@gmail.com>
2019-03-19 03:43:36 +0300

    good formatting for currency values

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields"/>
+    <b-table responsive dark hover :items="items" :fields="fields">
+    </b-table>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import numeral from 'numeral';
 
 export default {
   name: 'Table',
   data() {
     return {
+      fields: {
+        name: { label: 'Name', sortable: true },
+        priceUsd: { label: 'Price', sortable: true, formatter: 'formatCurrency' },
+        marketCapUsd: { label: 'Market Cap', sortable: true, formatter: 'formatCurrency' },
+        volumeUsd24Hr: { label: 'Volume (24h)', sortable: true, formatter: 'formatCurrency' },
+      },
       items: [],
     };
   },
   async created() {
     const { data } = await axios.get('https://api.coincap.io/v2/assets?limit=15');
     this.items = data.data;
+  },
+  methods: {
+    formatCurrency(value) {
+      return numeral(value).format('($0.00a)');
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+/* eslint-disable import/no-extraneous-dependencies */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
